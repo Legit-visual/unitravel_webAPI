3fb4735c0eed16931d86bec1268bbce76a603dec
Martin Richter <richtermartin@outlook.cz>
2025-10-27 14:32:35 +0100

    Revert "test commit to see if it works"
    This reverts commit 8ef180ad581b56d4e30d6e2f37db4e8c15227557.

--- unitravel_webAPI/Program.cs
+++ unitravel_webAPI/Program.cs
@@ -12,7 +12,7 @@ namespace unitravel_webAPI
             string API_USERNAME = Environment.GetEnvironmentVariable("API_USERNAME") ?? string.Empty;
             string API_PASSWORD = Environment.GetEnvironmentVariable("API_PASSWORD") ?? string.Empty;
 
-            // idk something really interesting blah blah blah
+            // idk something
             // Add services to the container.
 
             builder.Services.AddControllers();
