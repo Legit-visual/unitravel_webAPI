92488cd0999bd725b8b64726d923f7912e6e900d
Andrew <andrewmunoz196@gmail.com>
2025-10-27 14:06:37 +0100

    Added .env support

--- unitravel_webAPI/Program.cs
+++ unitravel_webAPI/Program.cs
@@ -1,4 +1,4 @@
-
+using DotNetEnv;
 namespace unitravel_webAPI
 {
     public class Program
@@ -7,13 +7,18 @@ namespace unitravel_webAPI
         {
             var builder = WebApplication.CreateBuilder(args);
 
+            // Adding .env suuport
+            DotNetEnv.Env.Load();
+            string API_USERNAME = Environment.GetEnvironmentVariable("API_USERNAME") ?? string.Empty;
+            string API_PASSWORD = Environment.GetEnvironmentVariable("API_PASSWORD") ?? string.Empty;
+
             // Add services to the container.
 
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
-            var app = builder.Build();
+            var app = builder.Build();  
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
