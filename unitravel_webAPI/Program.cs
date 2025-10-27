426d4dd08d686315495ed3066e07ba60f70383c7
Martin Richter <richtermartin@outlook.cz>
2025-10-27 14:17:38 +0100

    test commit

--- unitravel_webAPI/Program.cs
+++ unitravel_webAPI/Program.cs
@@ -12,6 +12,7 @@ namespace unitravel_webAPI
             string API_USERNAME = Environment.GetEnvironmentVariable("API_USERNAME") ?? string.Empty;
             string API_PASSWORD = Environment.GetEnvironmentVariable("API_PASSWORD") ?? string.Empty;
 
+            // idk something
             // Add services to the container.
 
             builder.Services.AddControllers();
